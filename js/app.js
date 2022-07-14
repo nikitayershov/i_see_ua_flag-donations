510752f6a95902848c841b310867413c1391214b
Nikita Yershov <nikita.yershov@gmail.com>
2022-07-15 00:34:09 +0300

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+
+    loop: true,
+
+
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
